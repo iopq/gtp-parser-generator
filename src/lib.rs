3528544b30d5895320cb7082cb2ea9b84e218434
Igor Polyakov <iopguy@gmail.com>
2017-04-11 01:59:21 -0700

    from parsed command added

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,51 @@ macro_rules! first {
     ( $a:tt $(, $rest:tt)* ) => ( $a )
 }
 
+macro_rules! replace_expr {
+    ($_t:tt $sub:expr) => {$sub};
+}
+
 macro_rules! example {
 
     (enum $enumer:ident { $($i:ident => $e:tt $( ( $($m:ident),* ) )* ; )* } ) => {
         pub enum $enumer {
         $(
-            $i $( ( ( $($m),* ) ) )*,
+            $i $( ( ( $($m),*, ) ) )*,
         )*
         }
-        
         
         impl $enumer {
             pub fn as_str(&self) -> &str {
                 match *self {
                     $(
                         $enumer :: $i $( ( first!(_ $(, $m)* ) ) )* => $e,
-
                     )*
+                }
+            }
+            
+            pub fn from_parsed_command(parsed: Command) -> Result<Self, ParseError> {
+                match parsed.name {
+                    $(
+                        //$e and $i level
+                        $e => Ok($enumer::$i $(  ( { let mut iter = parsed.args.into_iter(); (
+                            $( replace_expr!($m iter.next().unwrap().parse().unwrap()), )*
+                        )   } ) )* ),
+                        
+                    )*
+                    _ => Err(ParseError{})
                 }
             }
         }
     };
+}
+
+pub struct Command<'a> {
+    name: &'a str,
+    args: Vec<&'a str>
+}
+
+pub struct ParseError {
+
 }
 
 example!(enum Foo { One => "two"; });
@@ -30,6 +54,7 @@ example!(enum Foo { One => "two"; });
 example!(enum Bar {
     Foo => "bar" (String, String);
 });
+
 example!(enum Baz {
    First => "kek" (i32, String);
    Second => "wow" (i32, i32);
