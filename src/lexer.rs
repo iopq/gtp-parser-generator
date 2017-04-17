40affecfcea044314193f3ea5f76939a33f142c9
Igor Polyakov <iopguy@gmail.com>
2017-04-16 23:29:44 -0700

    whitespace

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,12 +15,12 @@ fn word() -> Parser<u8, String> {
 }
 
 fn non_control_non_space_non_comment(term: u8) -> bool {
-	term > 32 && term != 127 && term != b'#'
-	//control characters in ascii and space is 32
+    term > 32 && term != 127 && term != b'#'
+    //control characters in ascii and space is 32
 }
 
 fn utf8(v: Vec<u8>) -> String {
-	String::from_utf8(v).unwrap()
+    String::from_utf8(v).unwrap()
 }
 
 pub fn command() -> Parser<u8, Vec<String>> {
@@ -29,7 +29,7 @@ pub fn command() -> Parser<u8, Vec<String>> {
 
 #[test]
 fn command_comment() {
-	let input = ::pom::DataInput::new(b" \tfoo\t\t   bar#comment here");
+    let input = ::pom::DataInput::new(b" \tfoo\t\t   bar#comment here");
     let output = command().parse(&mut input.clone());
-	assert_eq!(output, Ok(vec!["foo".to_string(), "bar".to_string()]));
+    assert_eq!(output, Ok(vec!["foo".to_string(), "bar".to_string()]));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -21,17 +21,17 @@ pub struct ParseError {
 commands!(enum Gtp { Quit => "quit"; });
 
 pub fn try_parse(input: &[u8]) -> Result<Command, ParseError> {
-	let input = DataInput::new(input);
+    let input = DataInput::new(input);
     let parse = lexer::command().parse(&mut input.clone());
-	let mut parse = parse.unwrap();
+    let mut parse = parse.unwrap();
 
-	let rest = parse.split_off(1);
-	let ref first = parse[0];
-	Ok(Command { name: first.clone(), args: rest })
+    let rest = parse.split_off(1);
+    let ref first = parse[0];
+    Ok(Command { name: first.clone(), args: rest })
 }
 
 #[test]
 fn parse_quit() {
     let quit = try_parse(b"quit");
-	assert_eq!(quit, Ok(Command{ name: "quit".to_string(), args: Vec::new() }));
+    assert_eq!(quit, Ok(Command{ name: "quit".to_string(), args: Vec::new() }));
 }
